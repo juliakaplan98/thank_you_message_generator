f6065a2be347627d44a523b5c8fa64fc2a1ff8fc
Julia Kaplan <juliakaplan98@outlook.com>
2024-02-14 14:51:37 -0500

    generate unique id per party group

--- party_data.py
+++ party_data.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from pathlib import Path
+import uuid
 import os
 import csv
 
 class Group:
-    def __init__(self, address, email, phone, gift):
+    def __init__(self, id, address, email, phone, gift):
         self.guest_names = []
+        self.id = id
         self.address = address
         self.email = email
         self.phone = phone
@@ -20,6 +22,9 @@ class Group:
     def get_attendance(self):
         return self.attendance
 
+# Group identification number
+group_id = uuid.uuid4()
+
 # Get number of guest per group
 group_size = int(input("Number of guests in this group? ")) 
 
@@ -30,7 +35,7 @@ group_phone = input("Party's Phone Number: ")
 group_gift = input("Gift Received: ")
 
 # Create group    
-new_group = Group(group_address, group_email, group_phone, group_gift)
+new_group = Group(group_id, group_address, group_email, group_phone, group_gift)
 
 # Add names of guests to group
 count=0
@@ -47,28 +52,24 @@ attendance = new_group.get_attendance()
 
 
 # data for dataframe
-data = {'Party Members': new_group.guest_names,
+data = {'ID': new_group.id,
+        'Party Members': new_group.guest_names,
         'Address': new_group.address,
         'Email': new_group.email,
         'Phone Number': new_group.phone,
         'Attendance Status': new_group.attendance,
         'Gift': new_group.gift}
 
-df = pd.DataFrame(data, columns=['Party Members', 'Address', 'Email', 'Phone Number', 'Attendance Status', 'Gift'])
+df = pd.DataFrame(data, columns=['ID', 'Party Members', 'Address', 'Email', 'Phone Number', 'Attendance Status', 'Gift'])
 path = Path('GuestList.csv')
 
 if path.is_file() == False:
-    # df = pd.DataFrame(data, columns=['Party Members', 'Address', 'Email', 'Phone Number', 'Attendance Status', 'Gift'])
     with open('GuestList.csv', 'w') as csvFile:
         df.to_csv('GuestList.csv', index=False,header=True)
         csvFile.close()
         print("CSV file created successfully.")
 
 else:
-    # df = pd.DataFrame(data)
-    # with open('GuestList.csv', 'a') as csvFile:
     df.to_csv('GuestList.csv', mode='a', index=False, header=False)
-        # df.to_csv('GuestList.csv')
-        # csvFile.close()
     print("Data appended successfully.")
 
