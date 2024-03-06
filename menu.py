5b02c6bafed9f14fd174e4635d7ab9077a0d0827
Julia Kaplan <juliakaplan98@outlook.com>
2024-03-05 20:49:25 -0500

    Add menu

---
+++ menu.py
@@ -0,0 +1,42 @@
+menu_main = """
+        ________________________________________________________________
+
+        Main Menu
+        ________________________________________________________________
+
+        1. Add New Event
+        2. Edit Event
+        3. Generate Thank You Message For Event Guests
+        3. Exit
+"""
+
+menu_add_new_event = """
+        ________________________________________________________________
+
+        Add New Event
+        ________________________________________________________________
+
+        1. Name
+        2. Type
+        3. Address
+        4. Date
+        5. Parties
+        6. Exit
+
+"""
+
+menu_edit_event = """
+        ________________________________________________________________
+
+        Edit Event
+        ________________________________________________________________
+
+        1. Change Name
+        2. Change Date
+        3. Change Type
+        4. Change Address
+        5. Change Paries
+        6. Exit
+
+
+"""
